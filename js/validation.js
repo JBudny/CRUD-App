5000949eee959f00c819454de32b6fa0c9f83a7e
JBudny <jbudny8@gmail.com>
2020-01-21 21:04:34 +0100

    Add input validation

---
+++ js/validation.js
@@ -0,0 +1,52 @@
+const formInputs = document.querySelectorAll('form input');
+let validationState = [0, 0, 0];
+
+formInputs.forEach(input => {
+    input.addEventListener('keyup', e => {
+        const addBtn = document.getElementById('submit-employee');
+        validationResult = validate(input, e, validationState);
+
+        if (validationResult) {
+            addBtn.removeAttribute('disabled');
+        } else {
+            addBtn.setAttribute('disabled', 'disabled');
+        }
+    });
+});
+
+const validateEmail = (input, email) => {
+    const emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailReg.test(email)) {
+        input.className = 'form-control is-valid';
+        return true;
+    }
+    input.className = 'form-control is-invalid';
+    return false;
+};
+
+const validateName = (input, name) => {
+    if (name.length >= 2) {
+        input.className = 'form-control is-valid';
+        return true;
+    }
+    input.className = 'form-control is-invalid';
+    return false;
+};
+
+const validate = (input, e, validationState) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+        case 'first-name':
+            validationState.splice(0, 1, validateName(input, value));
+            break;
+        case 'last-name':
+            validationState.splice(1, 1, validateName(input, value));
+            break;
+        case 'email':
+            validationState.splice(2, 1, validateEmail(input, value));
+            break;
+    }
+
+    return validationState.every(el => el);
+};
